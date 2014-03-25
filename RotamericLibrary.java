858de9d059bbf2383abbd35628bbf7b39324c828
Artur Meller <artur.meller.1994@gmail.com>
2014-03-25 12:30:17 -0400

    added code to read in rotameric library

--- RotamericLibrary.java
+++ RotamericLibrary.java
@@ -6,10 +6,47 @@ public class RotamericLibrary extends SideChainRotamerLibrary {
     //data storage is accomplished using linked hash map from back bone angles to each rotamer (list of chis) 
     LinkedHashMap<BackBoneAngles, DiscreteProbabilityDistribution> dataset; 
     
-    public RotamericLibrary(String fileName)
+    public RotamericLibrary(AminoAcid aminoAcid)
     {
 	//read in library for specific amino acid
-	//creates LinkedHashMap between backbone angles and discrete probability distribution 
+	//creates LinkedHashMap between backbone angles and discrete probability distribution
+
+	
+        List<String> list = new ArrayList<String>();
+        String filenameString = "rotamer_library/" + aminoAcid.getName().toLowerCase() + ".bbdep.rotamers.lib";
+        //System.out.println(System.getProperty("user.dir"));
+
+        //Read in entire file
+        Scanner thisFile = null;
+	dataset = new  new LinkedHashMap<BackBoneAngles,DiscreteProbabiltiyDistribution>(); 
+
+        try {
+            thisFile = new Scanner(new FileReader(filenameString));
+            while (thisFile.hasNextLine())
+                {
+                    String currentLine = thisFile.nextLine();
+                    
+		    //parse currentLine to access data fields
+		    List<String> parts = new ArrayList<String>();
+		    StringTokenizer st = new StringTokenizer(s, " ", false);
+		    while(st.hasMoreTokens())
+			parts.add(st.nextToken());
+		    
+		    //check to see if first entry is an amino acid name
+		    if(parts.get(0).equals(aminoAcid.getName().toUpperCase())
+		       {
+			   //create BackBone angles with parts.get(1) and parts.get(2)
+			   
+			   //put new entry into Linked Hash Map for those BackBone angles
+			   //create DiscreteProbabilityDataSet object
+			   //Chi values are in columns 9, 10, 11, 12 and probability is in column 8
+                }
+        }
+        catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        
     }
 
     public DiscreteProbabilityDistribution map(Double psi, Double phi)
