1e4a44c68fb0c3f5f68604218a2d063d4a8d35cb
Artur Meller <artur.meller.1994@gmail.com>
2014-03-26 14:09:23 -0400

    wrote out implementation of Rotameric Library, does not compile

--- RotamericLibrary.java
+++ RotamericLibrary.java
@@ -7,6 +7,10 @@ public class RotamericLibrary extends SideChainRotamerLibrary
     //data storage is accomplished using linked hash map from back bone angles to each rotamer (list of chis) 
     LinkedHashMap<BackboneAngles, DiscreteProbabilityDistribution> dataset; 
     
+    /**
+     * creates RotamericLibrary by reading in filename associated with amino acid
+     * @param is an amino acid for which to create rotameric library
+     */
     public RotamericLibrary(AminoAcid aminoAcid)
     {
 	//read in library for specific amino acid
@@ -14,18 +18,21 @@ public class RotamericLibrary extends SideChainRotamerLibrary
 
 	
         List<String> list = new ArrayList<String>();
-        String filenameString = "rotamer_library/" + aminoAcid.getName().toLowerCase() + ".bbdep.rotamers.lib";
+        String filenameString = aminoAcid.getFilename();
         //System.out.println(System.getProperty("user.dir"));
 
         //Read in entire file
         Scanner thisFile = null;
-	    dataset = new LinkedHashMap<BackBoneAngles,DiscreteProbabiltiyDistribution>(); 
-
+	dataset = new LinkedHashMap<BackBoneAngles,DiscreteProbabiltiyDistribution>(); 
+	    
         try {
             thisFile = new Scanner(new FileReader(filenameString));
 	    
-	    double currPhi = 0.0;
-	    double currPsi = 0.0;
+	    double currPhi = -180.0;
+	    double currPsi = -180.0;
+
+	    List<List<Double>> tempChis = new ArrayList<>();
+	    List<Double> tempProbabilities = new ArrayList<>();
 
             while (thisFile.hasNextLine())
                 {
@@ -41,21 +48,35 @@ public class RotamericLibrary extends SideChainRotamerLibrary
 		    if (parts.get(0).equals(aminoAcid.getName().toUpperCase()))
 		       {
 			   //create BackBone angles with parts.get(1) and parts.get(2)
-			   double nextPhi = parts.get(1);
-			   double nextPsi = parts.get(2);
+			   double nextPhi = Double.parseDouble(parts.get(1));
+			   double nextPsi = Double.parseDouble(parts.get(2));
 			   
 			   if (currPsi == nextPsi && currPhi == nextPhi) {
 			       //add to temporary list of list of chis and probabilites
-			       
+			       List<Double> chis = new ArrayList<Double>();
+			       //Chi values are in columns 9, 10, 11, 12 and probability is in column 8
+			       chis.add(parts.get(9));
+			       chis.add(parts.get(10));
+			       chis.add(parts.get(11));
+			       chis.add(parts.get(12));
+			       tempChis.add(chis);
+			       tempProbabilites.add(parts.get(8));
 			   }
 			   else {
-			       //create new linked hash map entry
-			       
+			       //create new backboneAngles object
+			       BackboneAngles backboneAngles = new BackboneAngles(currPsi, currPhi);
+			       //create DiscreteProbabilityDataSet object
+			       DiscreteProbilityDistribution dpd = new DiscreteProbabilityDistribution(tempChis, tempProbabilities);
+			       //put new entry into Linked Hash Map for those BackBone angles
+			       dataset.put(backboneAngles, dpd);
+			       //reset for next round 
+			       currPsi = nextPsi;
+			       currPhi = nextPhi;
+			       tempChis = null;
+			       tempProbabilities = null;
 			       
 			   }
-			   //put new entry into Linked Hash Map for those BackBone angles
-			   //create DiscreteProbabilityDataSet object
-			   //Chi values are in columns 9, 10, 11, 12 and probability is in column 8
+			   
                 }
                 }
             }
@@ -68,11 +89,19 @@ public class RotamericLibrary extends SideChainRotamerLibrary
         
     }
 
-    public DiscreteProbabilityDistribution map(Double psi, Double phi)
+    /** returns the DiscreteProbabilityDistribution associated with psi and phi
+     * @return a DPD object with list of chis as outcomes and associated probabilities for a psi and phi
+     */
+    public DiscreteProbabilityDistribution get(Double psi, Double phi)
     {
-        return null;
+         return dataset.get(BackboneAngles(psi,phi));
 	//return discrete probability distribution for psi and phi
 	//allows user to then get random rotamer from that discrete probability distribution
     }
 
+    //for testing purposes
+    public static void main(String[] args) {
+	RotamericLibrary rotLib = new RotamericLibrary(AminoAcid.ASN);
+	System.out.println(dataset);
+    }
 }
